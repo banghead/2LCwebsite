e69582192e1dc92230f909d19f224e487b88bd23
podroks <bouilleteauflorian@outlook.fr>
2022-12-18 14:36:20 +0100

    color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      "primary": "#e5e7eb",
+      "secondary-light": "#d1fae5",
+      "secondary-dark": "#10b981",
+      "secondary": "#d1fae5",
+      "thertiary": "#111827"
+    },
     extend: {},
   },
   plugins: [],
