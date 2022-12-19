8c0fd0663a8dd0e80284ace2fe16632c9d7f5a01
banghead <37782667+banghead@users.noreply.github.com>
2022-12-19 16:30:05 +0100

    Update AdhesionBanner.js

--- src/components/AdhesionBanner/AdhesionBanner.js
+++ src/components/AdhesionBanner/AdhesionBanner.js
@@ -4,14 +4,14 @@ import Button from "../Button/Button";
 
 function AdhesionBanner() {
     return (
-        <div className="bg-secondary-light mt-[4.5em] mr-20 ml-20">
+        <div className="bg-tertiary text-text-tertiary mt-[4.5em] mr-20 ml-20 ">
             <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
                 <div className="flex flex-wrap items-center justify-between ">
                     <div className="flex w-0 flex-1 items-center">
-            <span className="flex rounded-lg bg-secondary-dark p-2">
+            <span className="flex rounded-lg bg-tertiary p-2 border">
               <MegaphoneIcon className="h-6 w-6 text-text-tertiary" aria-hidden="true"/>
             </span>
-                        <p className="ml-3 truncate font-medium text-tertiary">
+                        <p className="ml-3 truncate font-medium text-text-tertiary">
                             <span className="md:hidden">Vous pouvez adhérer!</span>
                             <span
                                 className="hidden md:inline">Bonne nouvelle ! les adhésions sont ouvertes.</span>
