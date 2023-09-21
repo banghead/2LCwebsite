682cdcb15c1d1a3785e48c0979d8d047330eb4ad
Florian <florian.aubin974@gmail.com>
2023-09-21 17:36:52 +0200

    fix shadow

--- src/components/MainCard/MainCard.js
+++ src/components/MainCard/MainCard.js
@@ -7,18 +7,18 @@ function MainCard({image, title, title2, details}) {
                 <div className="grid md:grid-cols-3 grid-cols-1 shadow-lg overflow-hidden rounded">
 
                     <div className="w-full">
-                        <div className="flex-1 bg-white rounded-r rounded-b-none overflow-hidden shadow-lg">
+                        <div className="flex-1 bg-white rounded-r rounded-b-none overflow-hidden shadow-lg h-full">
                             <h1 className="w-full font-bold text-xl text-gray-900 px-6 pt-6 pb-2">{title}</h1>
                             {details}
                         </div>
                     </div>
 
-                    <div className="h-full flex justify-center items-center shadow-lg">
-                        <img src={image} alt={title} className=" p-5"/>
+                    <div className="h-full flex justify-center items-center">
+                        <img src={image} alt={title} className="p-5"/>
                     </div>
 
                     <div className="w-full">
-                        <div className="flex-1 bg-white rounded-r rounded-b-none overflow-hidden shadow-lg">
+                        <div className="flex-1 bg-white rounded-r rounded-b-none overflow-hidden shadow-lg h-full">
                             <h1 className="w-full font-bold text-xl text-gray-900 px-6 pt-6 pb-2">{title2}</h1>
                             {details}
                         </div>
